724d1061dc88dbd37d0876d2dc8d8d14e2393f31
likuan <likuan@byosoft.com.cn>
2022-12-07 14:43:35 +0800

    add floder

---
+++ EasyLogger/easylogger/inc/test.h
@@ -0,0 +1,7 @@
+#ifndef _TEST_H_
+#define _TEST_H_
+#include <elog_cfg.h>
+
+void test_Func(int num);
+
+#endif
---
+++ EasyLogger/easylogger/src/test.c
@@ -0,0 +1,9 @@
+#include <test.h>
+#include <elog.h>
+
+#define MAXSIZE 100
+void test_Func(int num){
+    
+    log_a("第%d次打印 : Hello EasyLogger!", num);
+
+}
