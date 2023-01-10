b9f58b8922a8196cc8bc4fa90ee463d90220ba57
foreveryoung <1243288336@qq.com>
2023-01-10 11:11:13 +0800

    添加测试文件

--- KATA_Practise/4_Test.c
+++ KATA_Practise/4_Test.c
@@ -7,80 +7,12 @@
  * @FilePath: \C\.vscode\KATA_Practise\4_Test.c
  */
 #include <stdio.h>
-int* twoSum(int* nums, int numsSize, int target, int* returnSize);
-int main () {
-    int x = 121;
-    int rawvalue = x;
-    int temp =0;
-    if (x < 0)
-    {
-      return  0;
-
-    } else {
-      while (x > 0)
-      {
-        temp = temp * 10  + x % 10 ;
-        x /= 10 ;
-      }     
-    }
-     if (rawvalue == temp)
-      {
-        printf("sucess");
-      } 
-       return 0;
-  
+#define MAX_SIZE 10
+int main()
+{
+	for (int i = 0; i < MAX_SIZE; i++)
+	{
+		printf("第%d次, hello world!\n", i + 1);
+	}
+	return 0;
 }
-/**
- * @description: 返回数组中相加等于目标值的下标
- *                注意要使用动态分配内存存放数据
- * @param {int*} nums
- * @param {int} numsSize
- * @param {int} target
- * @param {int*} returnSize
- * @return {*}
- */
-int* twoSum(int* nums, int numsSize, int target, int* returnSize){
-
-  int * ptr = returnSize;
-  for (int i = 0; i < numsSize-1 ; i++ ) { 
-    for (int j = i + 1; j < numsSize ; j++) {
-      if (nums[i] + nums[j] == target)
-      {
-        *ptr = i;
-        ptr ++;
-        *ptr = j;
-      }
-      
-    }
-  }
-  return returnSize;
-
-}
-/**
- * @description: 
- * @param {*}
- * @return {*}
- */
-//复杂方法
-int digital_root(int n) {
-  
-  if (n < 10)  return n ;
-  int sum = 0 ;
-  while (n > 0)
-  {
-    sum += n % 10 ;
-    n /= 10; 
-  }
-  return digital_root(sum);
-  
-}
-/*
-简单方法
-比如num=1000*a+100*b+10*c+d；
-可以写成num=999*a+99*b+9*c+(a+b+c+d)；
-如果(a+b+c+d) > 10, 重复上述步骤。
-*/
-int digital_root(int n) {
-  return (n-1)%9 + 1;
-}
-;
